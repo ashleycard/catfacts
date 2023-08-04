0bccfbc61635c0b99fc558fa0f8ea2f7d49172ea
Ashley Card <ashleyxcard@gmail.com>
2023-08-04 22:11:52 +0100

    added nicer outputting

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,25 @@ struct Cli {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     let args = Cli::parse();
     let n = &args.amount;
+
     for _i in 1..=n.parse().unwrap() {
-        let resp = reqwest::get("https://meowfacts.herokuapp.com")
+        let mut resp = reqwest::get("https://meowfacts.herokuapp.com")
             .await?
             .json::<serde_json::Value>()
             .await?;
 
-        println!("{}", resp["data"]);
+        let b = &resp["data"].take();
+        let mut c = format!("{}", b);
+        c.remove(0);
+        c.remove(0);
+        let end = c.len();
+        c.remove(end-1);
+        c.remove(end-2);
+        println!("{}", c);
+
     }
     Ok(())
 }
